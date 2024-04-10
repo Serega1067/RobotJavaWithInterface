3c76776e99e7bcdf1d71440af747c482d3ed54c1
Serega1067 <serega1067@yandex.ru>
2024-04-10 12:51:05 +0700

    Git init, add files .gitignore, README, RobotListener

---
+++ README.md
@@ -0,0 +1,4 @@
+# Interfaces.
+We will expand the robot's capabilities - we will give 
+it the ability to tell someone that, it has started moving 
+forward and stopped.
---
+++ edu/javacourse/robot/RobotListener.java
@@ -0,0 +1,8 @@
+package edu.javacourse.robot;
+
+public interface RobotListener
+{
+    public void startMove(double x, double y);
+
+    public void endMove(double x, double y);
+}
